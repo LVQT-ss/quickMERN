7842fe38557d9e613f0b920c26a9c8c9fd5da318
LVQT <levietquocthinh@gmail.com>
2024-09-01 17:22:35 +0700

    complete CRUD in MERN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import CreateBooks from './pages/CreateBooks'
 import ShowBook from './pages/ShowBook'
-import Editbook from './pages/Editbook'
+import EditBook from './pages/EditBook'
 import DeleteBook from './pages/DeleteBook'
 
 const App = () => {
@@ -15,8 +15,8 @@ const App = () => {
       <Route path='/' element={<Home/>} />
       <Route path='/books/create' element={<CreateBooks/>} />
       <Route path='/books/details/:id' element={<ShowBook/>} />
-      <Route path='/books/eidt/:id' element={<Editbook/>} />
-      <Route path='/boooks/delete/:id' element={<DeleteBook/>} />
+      <Route path='/books/edit/:id' element={<EditBook/>} />
+      <Route path='/books/delete/:id' element={<DeleteBook/>} />
     </Routes>
   )
 }
