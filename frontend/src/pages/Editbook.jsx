9bbbbae5bb5c6d401c6116c8c88b6e9274480164
LVQT <levietquocthinh@gmail.com>
2024-09-01 22:16:13 +0700

    add notifo and bok modal . commplete MERN quick

--- frontend/src/pages/Editbook.jsx
+++ frontend/src/pages/Editbook.jsx
@@ -3,7 +3,7 @@ import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import axios from 'axios';
 import { useNavigate , useParams} from 'react-router-dom';
-
+import { useSnackbar } from 'notistack';
 
 const EditBook = () => {
   const [title, setTitle] = useState('');
@@ -11,7 +11,7 @@ const EditBook = () => {
   const [publishYear, setPublishYear] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-
+  const { enqueueSnackbar } = useSnackbar();
   const {id} = useParams();
   
   useEffect(() => {
@@ -40,11 +40,14 @@ const EditBook = () => {
       .put(`http://localhost:5555/books/${id}`, data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar('Book Edited successfully', { variant:'success' });
         navigate('/');
       })
       .catch((error) => {
         setLoading(false);
         // alert('An error happened. Please Chack console');
+        enqueueSnackbar('Error', { variant: 'error' });
+
         console.log(error);
       });
   };
